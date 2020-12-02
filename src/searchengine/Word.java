101b3dac0563694658337d3de9dec8b72fe26b03
iambaires <69988993+iambaires@users.noreply.github.com>
2020-12-02 12:27:41 -0800

    Update Word.java

--- src/searchengine/Word.java
+++ src/searchengine/Word.java
@@ -1,5 +1,18 @@
 package searchengine;
 
-public class Word {
+import java.util.LinkedList;
 
+public class Word {
+	
+	private String word;
+	private LinkedList<DocRef> references = new LinkedList<DocRef>();
+	
+	public Word(String word) {
+		this.word=word;
+	}
+	
+	public void addRef(DocRef ref)
+	{
+		references.add(ref);
+	}
 }
