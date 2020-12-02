148c4b920fbb24b1aae60f73bd2411efd410fc5b
iambaires <69988993+iambaires@users.noreply.github.com>
2020-12-02 12:05:48 -0800

    Update DocRef.java

--- src/searchengine/DocRef.java
+++ src/searchengine/DocRef.java
@@ -1,5 +1,35 @@
 package searchengine;
 
 public class DocRef {
+	private String docID;
+	private int frequency;
+	
+	public DocRef(String docID) {
+		this.docID=docID;
+		this.frequency=0;
+	}
+	
+	public void increment()
+	{
+		this.frequency++;
+	}
+
+	public String getDocID() {
+		return docID;
+	}
+
+	public void setDocID(String docID) {
+		this.docID = docID;
+	}
+
+	public int getFrequency() {
+		return frequency;
+	}
+
+	public void setFrequency(int frequency) {
+		this.frequency = frequency;
+	}
+	
+	
 
 }
