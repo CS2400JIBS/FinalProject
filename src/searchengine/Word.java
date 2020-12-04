63b3de66a84f602adbfc743dde9e9649420e953a
iambaires <69988993+iambaires@users.noreply.github.com>
2020-12-03 19:02:46 -0800

    Update Word.java

--- src/searchengine/Word.java
+++ src/searchengine/Word.java
@@ -33,14 +33,13 @@ public class Word {
 		return references.get(index-1);
 	}
 	
-<<<<<<< HEAD
 	public String getWord()
 	{
-	   return word;
-=======
+	   return this.word;
+	}
+	
 	public int getDocFrequency() {
 		return references.size();
->>>>>>> 55f2d6b48229dfb5134d9a3ab1106e4207d2d7e9
 	}
 	
 }
